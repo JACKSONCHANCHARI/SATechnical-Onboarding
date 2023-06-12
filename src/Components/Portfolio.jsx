c0b0e81736978480dd594702bee3e95f61110b63
Jackson Paul Chanchari Curitima <JacksonPaul.ChanchariCuritima@studentambassadors.com>
2023-06-11 19:07:12 -0500

    Change 3

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -21,7 +21,7 @@ import React from "react";
  */
 import image from "../images/Tres.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Business discussion";
 
 /**
  * Project list
